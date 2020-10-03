755f4a9a0ebeac4d03819c7d91b70c10df03cd84
karraspierce09 <58855735+karraspierce09@users.noreply.github.com>
2020-10-03 10:01:54 -0400

    Add files via upload

---
+++ MenuItem/MenuItem/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuGB
+{
+    public class MenuItem
+    {
+
+        //Constructors
+        public MenuItem() { }
+
+        public MenuItem(int mealNum, string mealName, string description, string ingredients, decimal price)
+        {
+            MealNumber = mealNum;
+            MealName = mealName;
+            Description = description;
+            Ingredients = ingredients;
+            MealPrice = price;
+        }
+
+        //Properties
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public decimal MealPrice { get; set; }
+    }
+}
